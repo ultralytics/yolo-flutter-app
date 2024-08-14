dccf307988260de59a5ee54b92577da3d1903ce8
Ultralytics Assistant <135830346+UltralyticsAssistant@users.noreply.github.com>
2024-08-14 21:10:56 +0200

    Ultralytics Actions JSON, CSS and autolabel support (#51)
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- ios/Classes/ThresholdProvider.swift
+++ ios/Classes/ThresholdProvider.swift
@@ -5,25 +5,25 @@ import CoreML
 /// - Tag: ThresholdProvider
 /// Class providing customized thresholds for object detection model
 public class ThresholdProvider: MLFeatureProvider {
-    /// The actual NMS values to provide as input
-    var values = [
-        "iouThreshold": MLFeatureValue(double: 0.0),
-        "confidenceThreshold": MLFeatureValue(double: 0.0)
-    ]
+  /// The actual NMS values to provide as input
+  var values = [
+    "iouThreshold": MLFeatureValue(double: 0.0),
+    "confidenceThreshold": MLFeatureValue(double: 0.0),
+  ]
 
-    /// The feature names the provider has, per the MLFeatureProvider protocol
-    public var featureNames: Set<String> {
-        return Set(values.keys)
-    }
+  /// The feature names the provider has, per the MLFeatureProvider protocol
+  public var featureNames: Set<String> {
+    return Set(values.keys)
+  }
 
-    /// Initialize with default values
-    init(iouThreshold: Double = 0.4, confidenceThreshold: Double = 0.2) {
-        self.values["iouThreshold"] = MLFeatureValue(double: iouThreshold)
-        self.values["confidenceThreshold"] = MLFeatureValue(double: confidenceThreshold)
-    }
+  /// Initialize with default values
+  init(iouThreshold: Double = 0.4, confidenceThreshold: Double = 0.2) {
+    self.values["iouThreshold"] = MLFeatureValue(double: iouThreshold)
+    self.values["confidenceThreshold"] = MLFeatureValue(double: confidenceThreshold)
+  }
 
-    /// The actual values for the features the provider can provide
-    public func featureValue(for featureName: String) -> MLFeatureValue? {
-        return values[featureName]
-    }
+  /// The actual values for the features the provider can provide
+  public func featureValue(for featureName: String) -> MLFeatureValue? {
+    return values[featureName]
+  }
 }
