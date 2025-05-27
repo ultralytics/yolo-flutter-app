86adcc4d00ebd43ca0d9918ef2de7bc2f2e021d4
UltralyticsAssistant <web@ultralytics.com>
2025-05-27 08:35:26 +0000

    Auto-format by https://ultralytics.com/actions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -161,8 +161,9 @@ class _CameraInferenceScreenState extends State<CameraInferenceScreen> {
           // Controller Toggle Button
           IconButton(
             icon: Icon(_useController ? Icons.gamepad : Icons.key),
-            tooltip:
-                _useController ? 'Using Controller' : 'Using Direct Access',
+            tooltip: _useController
+                ? 'Using Controller'
+                : 'Using Direct Access',
             onPressed: () {
               setState(() {
                 _useController = !_useController;
