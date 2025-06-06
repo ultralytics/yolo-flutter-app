b5cadf8f7e6472dada75aa429d3ce4ea59271891
UltralyticsAssistant <web@ultralytics.com>
2025-06-06 17:39:54 +0000

    Auto-format by https://ultralytics.com/actions

--- screenshot_app/ios/Flutter/ephemeral/flutter_lldb_helper.py
+++ screenshot_app/ios/Flutter/ephemeral/flutter_lldb_helper.py
@@ -1,8 +1,11 @@
+# Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 #
 # Generated file, do not edit.
 #
 
 import lldb
+
 
 def handle_new_rx_page(frame: lldb.SBFrame, bp_loc, extra_args, intern_dict):
     """Intercept NOTIFY_DEBUGGER_ABOUT_RX_PAGES and touch the pages."""
@@ -13,13 +16,14 @@ def handle_new_rx_page(frame: lldb.SBFrame, bp_loc, extra_args, intern_dict):
     # first page to see if handled it correctly. This makes diagnosing
     # misconfiguration (e.g. missing breakpoint) easier.
     data = bytearray(page_len)
-    data[0:8] = b'IHELPED!'
+    data[0:8] = b"IHELPED!"
 
     error = lldb.SBError()
     frame.GetThread().GetProcess().WriteMemory(base, data, error)
     if not error.Success():
-        print(f'Failed to write into {base}[+{page_len}]', error)
+        print(f"Failed to write into {base}[+{page_len}]", error)
         return
+
 
 def __lldb_init_module(debugger: lldb.SBDebugger, _):
     target = debugger.GetDummyTarget()
@@ -27,6 +31,6 @@ def __lldb_init_module(debugger: lldb.SBDebugger, _):
     # BreakpointCreateByName. For some reasons callback function does not
     # get carried over from dummy target for the later.
     bp = target.BreakpointCreateByRegex("^NOTIFY_DEBUGGER_ABOUT_RX_PAGES$")
-    bp.SetScriptCallbackFunction('{}.handle_new_rx_page'.format(__name__))
+    bp.SetScriptCallbackFunction(f"{__name__}.handle_new_rx_page")
     bp.SetAutoContinue(True)
     print("-- LLDB integration loaded --")
