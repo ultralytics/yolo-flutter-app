4a610794c1b2899151e64737080b45666de5d069
UltralyticsAssistant <web@ultralytics.com>
2025-07-04 08:44:54 +0000

    Auto-format by https://ultralytics.com/actions

--- samples/04_classification/macos/Flutter/GeneratedPluginRegistrant.swift
+++ samples/04_classification/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,10 +1,11 @@
+// Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 //
 //  Generated file. Do not edit.
 //
 
 import FlutterMacOS
 import Foundation
-
 import file_selector_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
