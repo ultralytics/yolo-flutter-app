c554f2f93b0495c893e02d252018811a9d57f634
Abrish Sabri <55057228+asabri97@users.noreply.github.com>
2025-08-06 18:40:43 +0200

    chore: remove example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,7 @@
 // Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:ultralytics_yolo_example/presentation/screens/camera_inference_screen.dart';
-=======
-import 'use_gpu_example.dart';
->>>>>>> cdd4d63d (fix: analyzer)
 
 void main() {
   runApp(const MyApp());
