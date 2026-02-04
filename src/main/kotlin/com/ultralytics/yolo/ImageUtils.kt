45591d35d56027f5bac1616505d7ebc9a89668ff
pq <qzbysh@gmail.com>
2026-02-04 13:15:18 +0800

    修复 ImageUtils 编译错误

--- src/main/kotlin/com/ultralytics/yolo/ImageUtils.kt
+++ src/main/kotlin/com/ultralytics/yolo/ImageUtils.kt
@@ -3,24 +3,12 @@
 package com.ultralytics.yolo
 
 import android.graphics.*
-import androidx.camera.core.ImageProxy
-import java.io.ByteArrayOutputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import kotlin.math.abs
 import kotlin.math.max
 
 object ImageUtils {
-
-    /**
-     * Sample to convert ImageProxy to NV21 (BYTE array), then [YuvImage] -> [Bitmap]
-     */
-    @JvmStatic
-    fun toBitmap(imageProxy: ImageProxy): Bitmap? {
-        val nv21 = yuv420888ToNv21(imageProxy)
-        val yuvImage = YuvImage(nv21, ImageFormat.NV21, imageProxy.width, imageProxy.height, null)
-        return yuvImageToBitmap(yuvImage)
-    }
 
     /**
      * Returns a transformation matrix from one reference frame into another. Handles cropping (if
@@ -81,97 +69,6 @@ object ImageUtils {
         }
 
         return matrix
-    }
-
-    private fun yuvImageToBitmap(yuvImage: YuvImage): Bitmap? {
-        val out = ByteArrayOutputStream()
-        val success = yuvImage.compressToJpeg(
-            Rect(0, 0, yuvImage.width, yuvImage.height),
-            100,
-            out
-        )
-        if (!success) return null
-        val imageBytes = out.toByteArray()
-        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
-    }
-
-
-    private fun yuv420888ToNv21(imageProxy: ImageProxy): ByteArray {
-        val cropRect = imageProxy.cropRect
-        val pixelCount = cropRect.width() * cropRect.height()
-        val pixelSizeBits = ImageFormat.getBitsPerPixel(ImageFormat.YUV_420_888)
-        val outputBuffer = ByteArray(pixelCount * pixelSizeBits / 8)
-        imageToByteBuffer(imageProxy, outputBuffer, pixelCount)
-        return outputBuffer
-    }
-
-
-    private fun imageToByteBuffer(
-        imageProxy: ImageProxy,
-        outputBuffer: ByteArray,
-        pixelCount: Int
-    ) {
-        require(imageProxy.format == ImageFormat.YUV_420_888) {
-            "Input ImageProxy must be in YUV_420_888 format."
-        }
-
-        val imageCrop = imageProxy.cropRect
-        val imagePlanes = imageProxy.planes
-
-        for (planeIndex in imagePlanes.indices) {
-            val (outputStride, startOffset) = when (planeIndex) {
-                0 -> Pair(1, 0)               // Y
-                1 -> Pair(2, pixelCount + 1)  // U
-                2 -> Pair(2, pixelCount)      // V
-                else -> return
-            }
-
-            val plane = imagePlanes[planeIndex]
-            val planeBuffer: ByteBuffer = plane.buffer
-            val rowStride = plane.rowStride
-            val pixelStride = plane.pixelStride
-
-            val planeCrop = if (planeIndex == 0) {
-                imageCrop
-            } else {
-                Rect(
-                    imageCrop.left / 2,
-                    imageCrop.top / 2,
-                    imageCrop.right / 2,
-                    imageCrop.bottom / 2
-                )
-            }
-
-            val planeWidth = planeCrop.width()
-            val planeHeight = planeCrop.height()
-
-            val rowBuffer = ByteArray(rowStride)
-            var outputOffset = startOffset
-
-            val rowLength = if (pixelStride == 1 && outputStride == 1) {
-                planeWidth
-            } else {
-                (planeWidth - 1) * pixelStride + 1
-            }
-
-            for (row in 0 until planeHeight) {
-                planeBuffer.position(
-                    (row + planeCrop.top) * rowStride +
-                            planeCrop.left * pixelStride
-                )
-
-                if (pixelStride == 1 && outputStride == 1) {
-                    planeBuffer.get(outputBuffer, outputOffset, rowLength)
-                    outputOffset += rowLength
-                } else {
-                    planeBuffer.get(rowBuffer, 0, rowLength)
-                    for (col in 0 until planeWidth) {
-                        outputBuffer[outputOffset] = rowBuffer[col * pixelStride]
-                        outputOffset += outputStride
-                    }
-                }
-            }
-        }
     }
 
     /**
@@ -242,6 +139,4 @@ object ImageUtils {
         byteBuffer.rewind()
         return byteBuffer
     }
-
-
 }
